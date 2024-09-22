00ba7338f5eafa1bf59321dcfb20cafccfd929ba
RilloyWC18 <rilloy@gmail.com>
2024-09-23 09:58:09 +1200

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,9 @@
+Name=input("Enter your name: ")
+W=int(input("Enter the width: "))
+L=int(input("Enter the length: "))
+A=W*L
+print(A)
+if(A>20):
+    print("Welcome")
+else:
+    print("Go Home")
---
+++ testcounting.py
@@ -0,0 +1,7 @@
+A=30
+B=20
+product=(A*B)
+if (product<500):
+    print(product)
+else:
+    print(A+B)and(product)
