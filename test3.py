e6fa0da5c56021b4ebd4ed83601b4b3db0d91d2c
RilloyWC18 <rilloy@gmail.com>
2024-09-23 09:58:27 +1200

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,8 @@
+print("Rilloy")
+num_1=10
+num_2=15
+add=num_1+num_2
+print(add)
+sub=num_2-num_1
+avg=add/2
+print(avg)
--- test5.py
+++ test5.py
@@ -1,4 +1,26 @@
+# Calculate area
 W=10
 L=12
 A=L*W
 print(A)
+
+#Print name and sum
+
+print("Rilloy")
+num_1=10
+num_2=15
+add=num_1+num_2
+print(add)
+sub=num_2-num_1
+avg=add/2
+print(avg)
+
+#Entering bday
+
+name=input("Enter your name: ")
+dob=int(input("Enter date of birth: "))
+age=int(input("enter age: "))
+if age>20:
+    print("you old asf")
+else:
+    print("damn young blood")
